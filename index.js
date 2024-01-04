7a711ca36e84900fb4d30b0d4c4ce3f416d1cd6f
aruturu24 <aruturu24@hotmail.com>
2024-01-04 15:52:24 -0300

    FEAT: support webhook post

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 const express = require('express')
 const app = express()
+
+const bodyParser = require('body-parser')
+app.use(bodyParser.json())
+
+app.post('/webhook', (req, res) => {
+    console.log(req.body)
+    res.status(200).send("OK")
+})
+
 app.all('/', (req, res) => {
     console.log("Just got a request!")
     res.send('Yo!')
