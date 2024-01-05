7b21ce3d83ba41e66100c39b0b84c4b3381b53c5
aruturu24 <aruturu24@hotmail.com>
2024-01-05 09:31:12 -0300

    WORKING: Test forms response to object

--- index.js
+++ index.js
@@ -5,7 +5,18 @@ const bodyParser = require('body-parser')
 app.use(bodyParser.json())
 
 app.post('/webhook', (req, res) => {
-    console.log(req.body)
+    const request = req.body
+
+    const call = {
+        employee: req.body["Funcionário"],
+        problem: req.body["Tipo do problema/solicitação"],
+        priority: req.body["Prioridade"],
+        place: req.body["Onde você se encontra?"],
+        description: req.body["Descrição:"]
+    }
+
+    console.log(call)
+
     res.status(200).send("OK")
 })
 
@@ -14,3 +25,28 @@ app.all('/', (req, res) => {
     res.send('Yo!')
 })
 app.listen(process.env.PORT || 3000)
+
+
+/*const APIKey = "aaa"
+const APIToken = "aaa"
+
+const CardInfo = {
+    name: "teste",
+    desc: "# teste"
+}
+fetch(`https://api.trello.com/1/cards?idList=aaa&key=${APIKey}&token=${APIToken}`, {
+    method: 'POST',
+    headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(CardInfo)
+})
+    .then(response => {
+        console.log(
+            `Response: ${response.status} ${response.statusText}`
+        );
+        return response.text();
+    })
+    .then(text => console.log(text))
+    .catch(err => console.error(err));*/
